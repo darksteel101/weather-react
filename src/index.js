d616b7029cb4f21153539fa92a77aaa8bf70a229
darksteel101 <71864703+darksteel101@users.noreply.github.com>
2022-02-19 08:41:57 -0500

    copied css and project from codesandbox

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import Search from "./Search";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div class="wrapper">
+      <div class="container">
+        <Search />
+        <App />
+      </div>
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
